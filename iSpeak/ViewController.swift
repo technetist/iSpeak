6b26e41a2cb687682481a34021aa115db7d730f6
Adrien Maranville <Adrien.Maranville@gmail.com>
2017-07-16 22:52:03 -0600

    Added speech recognition from a file

--- iSpeak/ViewController.swift
+++ iSpeak/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let audioPath = Bundle.main.path(forResource: "test", ofType: "wav") {
+            let url = URL(fileURLWithPath: audioPath)
+            let recognizer = SFSpeechRecognizer()
+            let request = SFSpeechURLRecognitionRequest(url: url)
+            recognizer?.recognitionTask(with: request, resultHandler: { (result, error) in
+                if result != nil {
+                    print(result!.bestTranscription.formattedString)
+                    print(result!)
+                } else if error != nil {
+                    print(error!)
+                }
+            })
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
